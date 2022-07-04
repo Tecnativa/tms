3da881b0dbea52ae2a3cf4414d70584ecaf19cb3
sergio-teruel <sergio.teruel@tecnativa.com>
2022-07-04 17:35:22 +0200

    Initial modules

---
+++ sale_line_reassign/tests/test_sale_line_reassign.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.tests import SavepointCase
+
+
+class TestSaleLineReassign(SavepointCase):
+    def test_reassign(self):
+        partner = self.env["res.partner"].create({"name": "Partner Test",})
+        product = self.env["product.product"].create({"name": "Product Test",})
+        sale_from = self.env["sale.order"].create(
+            {
+                "partner_id": partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test Product",
+                            "product_id": product.id,
+                            "product_uom_qty": 2,
+                            "price_unit": 150.00,
+                            "product_uom": self.env.ref("uom.product_uom_unit").id,
+                        },
+                    )
+                ],
+            }
+        )
+        sale_to = self.env["sale.order"].create({"partner_id": partner.id,})
+        pl_reassign = (
+            self.env["sale.order.line.reassign.wiz"]
+            .with_context({"active_ids": sale_from.order_line.ids,})
+            .create({"sale_order_id": sale_to.id,})
+        )
+        pl_reassign.action_apply()
+        self.assertFalse(len(sale_from.order_line))
+        self.assertTrue(len(sale_to.order_line))
---
+++ sale_line_reassign/wizards/sale_line_reassign_wiz.py
@@ -0,0 +1,48 @@
+# Copyright 2018 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2018-2021 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from odoo import _, exceptions, fields, models
+
+
+class SaleOrderLineReassignWiz(models.TransientModel):
+    _name = "sale.order.line.reassign.wiz"
+    _description = "Wizard to reassign sale order lines to other sale order"
+
+    def _default_partner_id(self):
+        active_ids = self.env.context.get("active_ids")
+        lines = self.env["sale.order.line"].browse(active_ids)
+        partner = lines[:1].order_id.partner_id
+        if lines.filtered(lambda l: (l.invoice_lines or l.order_partner_id != partner)):
+            raise exceptions.ValidationError(
+                _("Selected line/s are invoiced or they have distinct " "customers")
+            )
+        return partner
+
+    sale_order_id = fields.Many2one(comodel_name="sale.order", string="Move to sale",)
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Customer",
+        default=lambda self: self._default_partner_id(),
+    )
+
+    def action_apply(self):
+        active_ids = self.env.context.get("active_ids")
+        lines = self.env["sale.order.line"].browse(active_ids)
+        project = self.sale_order_id.project_ids[:1]
+        lines.update({"order_id": self.sale_order_id.id})
+        # Update project in so lines and tasks after v12 changes
+        project_so_lines = lines.filtered("project_id")
+        if project_so_lines:
+            # Repeat update to avoid rewrite first line
+            if not project:
+                project = project_so_lines[:1]._timesheet_create_project()
+                project_so_lines[1:].update({"project_id": project.id})
+            else:
+                project_so_lines.update({"project_id": project.id})
+            project_so_lines.mapped("task_id").update({"project_id": project.id})
+        # If sale_order_line_vendor is installed
+        if "purchase_line_ids" in lines:
+            lines.mapped("purchase_line_ids").update(
+                {"account_analytic_id": self.sale_order_id.analytic_account_id.id}
+            )
+        return lines
