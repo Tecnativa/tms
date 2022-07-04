c729ec8ef9c9d549d253698f29b67560bf691cd9
sergio-teruel <sergio.teruel@tecnativa.com>
2022-07-04 17:35:22 +0200

    Initial modules

---
+++ purchase_line_reassign/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2018 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Purchase Line Reassign",
+    "summary": "",
+    "version": "13.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://github.com/OCA/tms",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["purchase",],
+    "data": ["wizards/purchase_line_reassign_view.xml",],
+}
