9b581e386d05cab7ca1706611c6676870013e86b
sergio-teruel <sergio.teruel@tecnativa.com>
2022-08-30 16:16:13 +0200

    [DCK] tms_port_community_system_order_type: New module

---
+++ tms_port_community_system_order_type/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Tecnativa - Sergio Teruel
+# Copyright 2022 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Port Community System (PCS) Sale order type",
+    "summary": "Port Community System (PCS) Sale order type",
+    "version": "15.0.1.0.0",
+    "category": "Logistic",
+    "website": "https://github.com/OCA/tms",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "tms_port_community_system",
+        "sale_order_type",
+    ],
+    "data": [
+        "views/tms_pcs_view.xml",
+    ],
+}
---
+++ tms_port_community_system_order_type/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to mapped sale order type to orders imported from Valencia Ports
