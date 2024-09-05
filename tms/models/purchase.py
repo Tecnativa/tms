1562bd210d5321f39261474e6cdf24980c36e1e7
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-09-05 10:11:57 +0200

    [FIX] tms: Variable move now is optional value in _prepare_account_move_line TT50717

--- tms/models/purchase.py
+++ tms/models/purchase.py
@@ -9,7 +9,7 @@ class PurchaseOrderLine(models.Model):
     _inherit = ["purchase.order.line", "tms.analytic"]
     _name = "purchase.order.line"
 
-    def _prepare_account_move_line(self, move):
-        vals = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
+    def _prepare_account_move_line(self, move=None):
+        vals = super(PurchaseOrderLine, self)._prepare_account_move_line(move=move)
         vals.update(self.env["tms.analytic"].analytic_fields_vals(self))
         return vals
